694580c7d2265fb4b8eb54c55d5607419edf5334
SAYDUR <saydur@labs-mac.local>
2020-01-14 15:18:03 +0600

    api are showing in textView using URL Session

--- ApiCall/ViewController.swift
+++ ApiCall/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var text_view: UITextView!
     @IBAction func call(_ sender: Any) {
-        
+        //API Call by urlsession.
         guard let url = URL(string: "https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233") else {
             return
         }
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
                 
             }
             if let data = data {
+                //for showing JESON in text view by converting in string
                 self.text_view.text = String(bytes: data, encoding: .utf8)
                 print(data)
                 
